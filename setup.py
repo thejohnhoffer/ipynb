a95b676802f41470a913434126f170c963e19020
thejohnhoffer <hoffexi@gmail.com>
2017-01-04 11:34:27 -0500

    get ready for pip

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ipynb',
+            version='0.1',
+            description='Import ipynb files to python',
+            url='http://github.com/thejohnhoffer/ipynb',
+            author='John Hoffer (thejohnhoffer)',
+            author_email='john@hoff.in',
+            license='MIT',
+            packages=['ipynb'],
+            zip_safe=False)
