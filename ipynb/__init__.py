1fc8f7bb1c7a5aae63ddfe4fadb272e0e19d9fa0
thejohnhoffer <hoffexi@gmail.com>
2017-01-04 11:43:13 -0500

    update from https://gist.github.com/DCAL12/ fork

--- ipynb/__init__.py
+++ ipynb/__init__.py
@@ -1,9 +1,10 @@
 """
 Module directly collated from
-http://nbviewer.ipython.org/github/adrn/ipython/blob/1.x/examples/notebooks/Importing%20Notebooks.ipynb
+http://jupyter-notebook.readthedocs.io/en/latest/examples/Notebook/Importing%20Notebooks.html
 """
 import io, os, sys, types
-from IPython.nbformat import current
+from IPython import get_ipython
+from nbformat import read
 from IPython.core.interactiveshell import InteractiveShell
 
 def find_notebook(fullname, path=None):
@@ -26,7 +27,7 @@ def find_notebook(fullname, path=None):
             return nb_path
 
 class NotebookLoader(object):
-    """Module Loader for IPython Notebooks"""
+    """Module Loader for Jupyter Notebooks"""
     def __init__(self, path=None):
         self.shell = InteractiveShell.instance()
         self.path = path
@@ -35,11 +36,12 @@ class NotebookLoader(object):
         """import a notebook as a module"""
         path = find_notebook(fullname, self.path)
 
-        print ("importing IPython notebook from %s" % path)
+        print ("importing Jupyter notebook from %s" % path)
 
         # load the notebook object
         with io.open(path, 'r', encoding='utf-8') as f:
-            nb = current.read(f, 'json')
+            nb = read(f, 4)
+
 
         # create the module and add it to sys.modules
         # if name in sys.modules:
@@ -47,6 +49,7 @@ class NotebookLoader(object):
         mod = types.ModuleType(fullname)
         mod.__file__ = path
         mod.__loader__ = self
+        mod.__dict__['get_ipython'] = get_ipython
         sys.modules[fullname] = mod
 
         # extra work to ensure that magics that would affect the user_ns
@@ -55,18 +58,18 @@ class NotebookLoader(object):
         self.shell.user_ns = mod.__dict__
 
         try:
-            for cell in nb.worksheets[0].cells:
-                if cell.cell_type == 'code' and cell.language == 'python':
-                    # transform the input to executable Python
-                    code = self.shell.input_transformer_manager.transform_cell(cell.input)
-                    # run the code in themodule
-                    exec code in mod.__dict__
+          for cell in nb.cells:
+            if cell.cell_type == 'code':
+                # transform the input to executable Python
+                code = self.shell.input_transformer_manager.transform_cell(cell.source)
+                # run the code in themodule
+                exec(code, mod.__dict__)
         finally:
             self.shell.user_ns = save_user_ns
         return mod
 
 class NotebookFinder(object):
-    """Module finder that locates IPython Notebooks"""
+    """Module finder that locates Jupyter Notebooks"""
     def __init__(self):
         self.loaders = {}
 
@@ -84,8 +87,7 @@ class NotebookFinder(object):
             self.loaders[key] = NotebookLoader(path)
         return self.loaders[key]
 
-# --------------
-
+## Aside: displaying notebooks
 from pygments import highlight
 from pygments.lexers import PythonLexer
 from pygments.formatters import HtmlFormatter
@@ -106,17 +108,18 @@ display(HTML("""
 def show_notebook(fname):
     """display a short summary of the cells of a notebook"""
     with io.open(fname, 'r', encoding='utf-8') as f:
-        nb = current.read(f, 'json')
+        nb = read(f, 4)
     html = []
-    for cell in nb.worksheets[0].cells:
+    for cell in nb.cells:
         html.append("<h4>%s cell</h4>" % cell.cell_type)
         if cell.cell_type == 'code':
-            html.append(highlight(cell.input, lexer, formatter))
+            html.append(highlight(cell.source, lexer, formatter))
         else:
             html.append("<pre>%s</pre>" % cell.source)
     display(HTML('\n'.join(html)))
 
 # Example
-#show_notebook(os.path.join("nbimp", "mynotebook.ipynb"))
+#show_notebook(os.path.join("nbpackage", "mynotebook.ipynb"))
 
+# Now we register the NotebookFinder with sys.meta_path
 sys.meta_path.append(NotebookFinder())
